e5cfc4ad86ec5a09064aaafc156918f71447a512
MacBookHiro <>
2020-02-01 23:46:53 +0900

    routeファイルを変更

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('usstocklist', 'UsstocklistsController');
-Route::get('/usstocklist', 'UsstocklistsController@index');
-Route::post('/usstocklist', 'UsstocklistsController@store');
-Route::get('/usstocklist/create', 'UsstocklistsController@create');
-Route::get('/usstocklist/{ticker}', 'UsstocklistsController@show');
-Route::put('/usstocklist/{ticker}', 'UsstocklistsController@update');
-Route::delete('/usstocklist/{ticker}', 'UsstocklistsController@destroy');
-Route::get('/usstocklist/{ticker}/edit', 'UsstocklistsController@edit');
+Route::resource('usstocklists', 'UsstocklistsController');
+Route::get('/usstocklists', 'UsstocklistsController@index');
+Route::post('/usstocklists', 'UsstocklistsController@store');
+Route::get('/usstocklists/create', 'UsstocklistsController@create');
+Route::get('/usstocklists/{ticker}', 'UsstocklistsController@show');
+Route::put('/usstocklists/{ticker}', 'UsstocklistsController@update');
+Route::delete('/usstocklists/{ticker}', 'UsstocklistsController@destroy');
+Route::get('/usstocklists/{ticker}/edit', 'UsstocklistsController@edit');
