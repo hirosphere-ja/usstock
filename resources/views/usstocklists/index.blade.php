9d46d3773da59be1ee117a35ae77f0921cdf5b53
MacBookHiro <>
2020-02-02 00:42:47 +0900

    viewファイルを変更

--- resources/views/usstocklists/index.blade.php
+++ resources/views/usstocklists/index.blade.php
@@ -3,5 +3,18 @@
 @section('title', '銘柄一覧')
 
 @section('content')
-<h2>aaa</h2>
+<table class="table">
+  <tr>
+    <th>ティッカー</th>
+    <th>銘柄名</th>
+    <th>市場</th>
+  </tr>
+  @foreach ($usstocklist as $usstocklist)
+  <tr>
+    <td>{{ strtoupper($usstocklist->ticker) }}</td>
+    <td>{{ $usstocklist->stockname }}</td>
+    <td>{{ $usstocklist->market_id }}</td>
+  </tr>
+  @endforeach
+</table>
 @endsection
