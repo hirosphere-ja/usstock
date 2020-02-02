d330fb979c2596ff5c1616dd2dac16ed78b69d21
MacBookHiro <>
2020-02-03 00:26:04 +0900

    modelファイルを変更 市場のリレーションを設定

--- app/Usstocklist.php
+++ app/Usstocklist.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Usstockmarket;
+
 class Usstocklist extends Model
 {
     // 文字列キーの場合、以下の２つの設定が必要
@@ -12,4 +14,10 @@ class Usstocklist extends Model
 
     // created_atやupdated_atを使わない設定
     public $timestamps = false;
+
+    protected $fillable = ['ticker', 'stockname', 'market_id'];
+
+    public function usstockmarket(){
+        return $this->belongsTo(Usstockmarket::class,'market_id','market_id');
+    }
 }
--- app/Usstockmarket.php
+++ app/Usstockmarket.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Usstocklist;
+
 class Usstockmarket extends Model
 {
-    //
+    protected $fillable = ['market_id', 'market'];
+
+    public function usstocklists(){
+        return $this->hasMany(Usstocklist::class);
+    }
 }
