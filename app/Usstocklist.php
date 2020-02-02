be3e41efafd476c62ff094b6fc4e59da03567242
MacBookHiro <>
2020-02-02 13:18:36 +0900

    modelファイルを更新

--- app/Usstocklist.php
+++ app/Usstocklist.php
@@ -9,4 +9,7 @@ class Usstocklist extends Model
     // 文字列キーの場合、以下の２つの設定が必要
     protected $keyType = 'string';
     protected $primaryKey = 'ticker';
+
+    // created_atやupdated_atを使わない設定
+    public $timestamps = false;
 }
