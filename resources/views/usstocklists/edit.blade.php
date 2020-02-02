0d961203399e0ca8ddce098b0a9adc9ab9c45ccc
MacBookHiro <>
2020-02-02 13:54:03 +0900

    viewファイルを作成

---
+++ resources/views/usstocklists/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title', '編集')
+
+@section('content')
+<form action="/usstocklists/{{ $usstocklist->ticker }}" method="post">
+    @csrf
+    <div>
+      <label for="ticker">ティッカー</label>
+      <input type="text" name="ticker" value="{{ $usstocklist->ticker }}">
+    </div>
+    <div>
+      <label for="ticker">銘柄名</label>
+      <input type="text" name="stockname" size="50" value="{{ $usstocklist->stockname }}">
+    </div>
+    <div>
+      <label for="market_id">銘柄名</label>
+      <select name="market_id">
+        <option value="1">NYSE</option>
+        <option value="2">NASDAQ</option>
+      </select>
+    </div>
+    <div>
+      <input type="hidden" name="_method" value="patch">
+      <input type="submit" value="送信">
+    </div>
+  </form>
+@endsection
