a076cbf8033fa74f0eee97ef8f47f83117c95447
MacBookHiro <>
2020-02-02 13:54:56 +0900

    routeファイルを更新

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::get('/usstocklists', 'UsstocklistsController@index');
 Route::post('/usstocklists', 'UsstocklistsController@store');
 Route::get('/usstocklists/create', 'UsstocklistsController@create');
 Route::get('/usstocklists/{ticker}', 'UsstocklistsController@show');
-Route::put('/usstocklists/{ticker}', 'UsstocklistsController@update');
+Route::post('/usstocklists/{ticker}', 'UsstocklistsController@update');
 Route::delete('/usstocklists/{ticker}', 'UsstocklistsController@destroy');
 Route::get('/usstocklists/{ticker}/edit', 'UsstocklistsController@edit');
