6ac2ecd6b4aca7ee96f920659f33c7c2bb8add27
MacBookHiro <>
2020-02-02 11:56:34 +0900

    modelファイルを修正

--- app/Usstocklist.php
+++ app/Usstocklist.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usstocklist extends Model
 {
-    //
+    // 文字列キーの場合、以下の２つの設定が必要
+    protected $keyType = 'string';
+    protected $primaryKey = 'ticker';
 }
