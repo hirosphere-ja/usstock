71abd1a55c3d38ef4194b033cb160f15c5605452
MacBookHiro <>
2020-02-03 20:57:14 +0900

    seederファイルに追加

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsstockmarketsTableSeeder::class);
         $this->call(UsstocklistsTableSeeder::class);
+        $this->call(UsstockdividendsTableSeeder::class);
     }
 }
