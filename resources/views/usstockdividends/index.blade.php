7f0c3e58fcc75c6a61f200309d0b28901972f5cb
MacBookHiro <>
2020-02-03 21:51:36 +0900

    viewファイルを作成

---
+++ resources/views/usstockdividends/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('title', '銘柄一覧')
+
+@section('content')
+<div>
+  <a href="/usstockdividends/create" class="btn btn-primary btn-sm">新規作成</a>
+</div>
+<table class="table">
+  <tr>
+    <th>ティッカー</th>
+    <th>発表日</th>
+    <th>権利落日</th>
+    <th>支払日</th>
+    <th>配当内容</th>
+    <th></th>
+  </tr>
+  @foreach ($usstockdividends as $usstockdividend)
+  <tr>
+    <td>{{ strtoupper($usstockdividend->ticker) }}</td>
+    <td>{{ date('Y/m/d',strtotime($usstockdividend->announceday)) }}</td>
+    <td>{{ date('Y/m/d',strtotime($usstockdividend->exrights)) }}</td>
+    <td>{{ date('Y/m/d',strtotime($usstockdividend->paymentday)) }}</td>
+    <td>{{ $usstockdividend->dividend }}米ドル</td>
+    <td>
+      <a href="/usstockdividends/{{ $usstockdividend->id }}" class="btn btn-primary btn-sm d-inline-block">詳細</a>
+      <a href="/usstockdividends/{{ $usstockdividend->id }}/edit" class="btn btn-primary btn-sm d-inline-block">編集</a>
+      <form action="/usstockdividends/{{$usstockdividend->id}}" method="post" class="d-inline-block">
+        @csrf
+        <input type="hidden" name="_method" value="delete">
+        <input type="submit" value="削除" class="btn btn-danger btn-sm btn-dell">
+      </form>
+    </td>  </tr>
+  @endforeach
+</table>
+@endsection
+@section('script')
+<script>
+  $(function(){
+    $(".btn-dell").click(function(){
+      if(confirm("本当に削除しますか？")){
+      // そのまま削除
+      }else{
+        return false;
+      }
+    });
+  });
+</script>
+@endsection
