46de60b2df1397b0299b3a5fbb0b691c2a0c5106
MacBookHiro <>
2020-02-04 23:27:18 +0900

    controllerファイルを変更

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,4 +11,19 @@ class IndexController extends Controller
     public function index(){
         return view('index');
     }
+
+    public function show(Request $request){
+        // キーワード受け取り
+        $ticker = $request->input('ticker');
+        // クエリ生成
+        $query = Usstockdividend::query();
+
+        // もしキーワードがあったら
+        if(!empty($ticker)){
+            $query->where('ticker',$ticker);
+        }
+
+        $usstockdividends = $query->orderBy('announceday','desc')->paginate(2);
+        return view('show')->with('usstockdividends',$usstockdividends)->with('ticker',$ticker);
+    }
 }
