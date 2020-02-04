dfc23851c5a3bb60350449268cb1c0854b44615c
MacBookHiro <>
2020-02-05 00:28:00 +0900

    viewファイルを変更

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,8 +1,20 @@
 @extends('layouts.app')
 
-@section('title', '銘柄詳細')
+@section('title', '個別銘柄現金配当')
 
 @section('content')
+<table class="table">
+  <tr>
+    <th>ティッカー</th>
+    <th>銘柄名</th>
+    <th>市場</th>
+  </tr>
+  <tr>
+    <td>{{ strtoupper($ticker) }}</td>
+    <td>{{ $usstocklist->stockname }}</td>
+    <td>{{ $usstocklist->usstockmarket->market }}</td>
+  </tr>
+</table>
 <table class="table">
   <tr>
     <th>発表日</th>
@@ -16,16 +28,18 @@
       <td>{{ date('Y/m/d',strtotime($usstockdividend->exrights)) }}</td>
       <td>{{ date('Y/m/d',strtotime($usstockdividend->paymentday)) }}</td>
       <td>{{ money_format("%.6n",$usstockdividend->dividend) }} USD</td>
+      <td>
+        <a href="/usstockdividends/{{ $usstockdividend->id }}/edit" class="btn btn-primary btn-sm d-inline-block">編集</a>
+        <form action="/usstockdividends/{{$usstockdividend->id}}" method="post" class="d-inline-block">
+          @csrf
+          <input type="hidden" name="_method" value="delete">
+          <input type="submit" value="削除" class="btn btn-danger btn-sm btn-dell">
+        </form>
+      </td>
     </tr>
   @endforeach
 </table>
-{{-- <a href="/usstockdividends/{{ $usstockdividend->id }}/edit" class="btn btn-primary btn-sm d-inline-block">編集</a>
-<form action="/usstockdividends/{{$usstockdividend->id}}" method="post" class="d-inline-block">
-  @csrf
-  <input type="hidden" name="_method" value="delete">
-  <input type="submit" value="削除" class="btn btn-danger btn-sm btn-dell">
-</form>
-<a href="/usstockdividends" class="btn btn-success btn-sm d-inline-block">一覧に戻る</a>
+<a href="/" class="btn btn-success btn-sm d-inline-block">TOPに戻る</a>
 @endsection
 @section('script')
 <script>
@@ -38,5 +52,5 @@
       }
     });
   });
-</script> --}}
+</script>
 @endsection
