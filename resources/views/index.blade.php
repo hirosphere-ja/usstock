6cab859f0f7bbb993eb58b1515be2eaabf527941
MacBookHiro <>
2020-02-04 19:42:19 +0900

    viewファイルを作成 TOPページを作成

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', '米国株式配当')
+
+@section('content')
+<div>
+  <a href="/usstocklists/" class="btn btn-primary">米国銘柄一覧</a>
+  <a href="/usstockdividends/" class="btn btn-primary">現金配当一覧</a>
+</div>
+@endsection
