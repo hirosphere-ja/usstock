dffc158134f4289b9645e676448e2a6222d0fab5
MacBookHiro <>
2020-02-04 23:25:49 +0900

    viewファイルを変更 検索フォームを追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,5 +6,13 @@
 <div>
   <a href="/usstocklists/" class="btn btn-primary">米国銘柄一覧管理</a>
   <a href="/usstockdividends/" class="btn btn-primary">現金配当一覧管理</a>
-</div>
+</div><br>
+<form action="show">
+  @csrf
+  <div>
+    <label for="ticker">ティッカー</label>
+    <input type="text" name="ticker">
+  </div>
+  <input type="submit" value="検索">
+</form>
 @endsection
