a135ade4b5796a6af18cd3bf0156fd396894274b
MacBookHiro <>
2020-02-08 15:43:42 +0900

    viewファイルの変更 バリデーションの適用 バリデーションエラー時の入力データを保持可能にする

--- resources/views/usstockdividends/create.blade.php
+++ resources/views/usstockdividends/create.blade.php
@@ -3,6 +3,15 @@
 @section('title', '新規作成')
 
 @section('content')
+  @if ($errors->any())
+  <div class="alert alert-danger">
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
   <form action="/usstockdividends" method="post">
     @csrf
     <div>
@@ -15,19 +24,19 @@
       </div>
     <div>
       <label for="announceday">発表日</label>
-      <input type="date" name="announceday" size="50" required>
+    <input type="date" name="announceday" size="50" value="{{ old('announceday') }}">
     </div>
     <div>
       <label for="exrights">権利落日</label>
-      <input type="date" name="exrights" size="50" required>
+      <input type="date" name="exrights" size="50" value="{{ old('exrights') }}">
     </div>
     <div>
       <label for="paymentday">支払日</label>
-      <input type="date" name="paymentday" size="50" required>
+      <input type="date" name="paymentday" size="50" value="{{ old('paymentday') }}">
     </div>
     <div>
       <label for="dividend">配当内容</label>
-      <input type="text" name="dividend" size="4" required>
+      <input type="text" name="dividend" size="4" value="{{ old('dividend') }}">
     </div>
     <div>
       <input type="submit" value="登録する" class="btn btn-primary">
