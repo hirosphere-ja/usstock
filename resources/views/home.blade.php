ceda49bc705ac658deb73a208f3327fc822003d8
MacBookHiro <>
2020-02-09 09:34:16 +0900

    viewファイルを変更 ログイン後にトップページへ戻るリンクボタンを追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     You are logged in!
+                    <a href="/" class="btn btn-success">TOPへ戻る</a>
                 </div>
             </div>
         </div>
