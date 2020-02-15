95d12c77a3fa07f3c3f81f89283729d3cef20998
MacBookHiro <>
2020-02-15 21:45:26 +0900

    viewファイルを変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>@yield('title')</title>
+
+    <!-- Google Adsence -->
     <script data-ad-client="ca-pub-2054344840815103" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
   </head>
   <body>
